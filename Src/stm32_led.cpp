0a4b5271c4ae2091974d947facc11327f80b4084
shimizu1ssei <shimizu070508@gmail.com>
2019-09-04 17:46:26 +0900

    Call GPIO_WritePin

--- Src/stm32_led.cpp
+++ Src/stm32_led.cpp
@@ -14,9 +14,11 @@ Led::Led(GPIO_TypeDef* GPIOx,uint16_t GPIO_Pin) {
 
 void Led::setOn(){
     state=LED_ON;
+    HAL_GPIO_WritePin(led_GPIOx,led_GPIO_Pin, GPIO_PIN_SET);
 }
 void Led::setOff(){
     state=LED_OFF;
+    HAL_GPIO_WritePin(led_GPIOx,led_GPIO_Pin, GPIO_PIN_RESET);
 }
 void Led::setFlash(){
     state=LED_FLASH;
