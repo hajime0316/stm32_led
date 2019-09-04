2af6cbcbc7b68d1778a51b18b4036adb4ffc6026
shimizu1ssei <shimizu070508@gmail.com>
2019-09-04 20:27:24 +0900

    Fix cppmain.cpp

--- Src/cppmain.cpp
+++ Src/cppmain.cpp
@@ -6,16 +6,19 @@
 
 #include "stm32_led.hpp"
 
-void setup(void) {
+Led led(GPIOD,GPIO_PIN_14);
 
+void setup(void) {
+	HAL_TIM_Base_Start_IT(&htim6);
+    led.setFlash();
 }
 
 void loop(void) {
-
+   
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
     if(htim->Instance == TIM6) {
-
+        led.interrut_toutine();
     }
 }
