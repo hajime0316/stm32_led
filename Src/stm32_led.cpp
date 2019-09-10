8c0f23c1904c27404cdbbbb6d45f0ffc1cff6f68
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-09-11 07:45:01 +0900

    Remove TODO comment

--- Src/stm32_led.cpp
+++ Src/stm32_led.cpp
@@ -67,7 +67,6 @@ void Stm32Led::setFlash(){
 }
 
 // TODO
-// 引数ありsetFlash()関数を作る
 // set_flash_periodをプライベート関数にする
 void Stm32Led::setFlash(unsigned int flash_period){
     set_flash_period(flash_period);
