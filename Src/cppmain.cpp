e17a1a928d766a6a085015ed11008b90f61d8c44
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-09-10 19:55:26 +0900

    Flash 4 LED

--- Src/cppmain.cpp
+++ Src/cppmain.cpp
@@ -7,9 +7,19 @@
 #include "stm32_led.hpp"
 
 void setup(void) {
-    static Stm32Led led(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+    static Stm32Led led_b(LED_B_GPIO_Port, LED_B_Pin, GPIO_PIN_SET);
+    static Stm32Led led_r(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_SET);
+    static Stm32Led led_o(LED_O_GPIO_Port, LED_O_Pin, GPIO_PIN_SET);
+    static Stm32Led led_g(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_SET);
+
+    // タイマスタート
 	HAL_TIM_Base_Start_IT(&htim6);
-    led.setFlash();
+
+    // LEDを点滅させる
+    led_b.setFlash();
+    led_r.setFlash();
+    led_o.setFlash();
+    led_g.setFlash();
 }
 
 void loop(void) {
