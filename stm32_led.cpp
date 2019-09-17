0fb6f3d28d6f890bc728a0f83333e2fc810f04eb
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-09-17 20:50:34 +0900

    Remove Lock Pin

--- stm32_led.cpp
+++ stm32_led.cpp
@@ -69,14 +69,12 @@ void Stm32Led::onTemporary(unsigned int temporary_time) {
     temporary_flag = 1; 
     this->temporary_time = temporary_time;
     HAL_GPIO_WritePin(led_GPIOx, led_GPIO_Pin, led_on_pin_state);
-    HAL_GPIO_LockPin(led_GPIOx, led_GPIO_Pin);
 }
 
 void Stm32Led::offTemporary(unsigned int temporary_time) {
     temporary_flag = 1; 
     this->temporary_time = temporary_time;
     HAL_GPIO_WritePin(led_GPIOx, led_GPIO_Pin, led_off_pin_state);
-    HAL_GPIO_LockPin(led_GPIOx, led_GPIO_Pin);
 }
 
 void Stm32Led::interrupt_handler() {
